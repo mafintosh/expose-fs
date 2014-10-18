2a45ae3f08026286e6f9746b762bb915ceb04b17
Mathias Buus <mathiasbuus@gmail.com>
2014-10-18 22:02:14 +0900

    add missing options

--- bin.js
+++ bin.js
@@ -1,22 +1,31 @@
 #!/usr/bin/env node
 
 var minimist = require('minimist')
+var fs = require('fs')
+var path = require('path')
 var createServer = require('./')
 
-var argv = minimist(process.argv.slice(2), {alias:{port:'p'}})
+var argv = minimist(process.argv.slice(2), {alias:{port:'p', quiet:'q', help:'h'}})
 var p = argv._[0] || '.'
 var port = argv.port || 8441
 
+if (argv.help) {
+  console.log(fs.readFileSync(path.join(__dirname, 'help.txt'), 'utf-8'))
+  return
+}
+
 var server = createServer(p)
 
-console.log('Exposing %s on port %d', server.root, port)
+if (!argv.quiet) {
+  console.log('Exposing %s on port %d', server.root, port)
 
-server.on('file', function(name) {
-  console.log('Returning file: %s', name)
-})
+  server.on('file', function(name) {
+    console.log('Returning file: %s', name)
+  })
 
-server.on('directory', function(name) {
-  console.log('Returning directory: %s', name)
-})
+  server.on('directory', function(name) {
+    console.log('Returning directory: %s', name)
+  })
+}
 
 server.listen(port)
